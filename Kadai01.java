8c0a41caaac835e835d530066a678bd04fde0a16
ryudama <kaihatsu.hidehiro.iida@gmail.com>
2023-05-01 13:32:26 +0900

    test1

--- Kadai01.java
+++ Kadai01.java
@@ -11,27 +11,11 @@ class Kadai01{
             Paths.get("/Users/iida/road_to_geek/java-kadai01/scores.csv"),
             StandardCharsets.UTF_8))
             {
-
-
-                ///Users/iida/road_to_geek/java-kadai01/kadai01.java
-                
-                //うまくいったときの処理がここ
-                /*
-                for(int i = 0; i < 999; i++){
-                    String line = r.readLine();
-                    if(line == null){
-                        break;
-                    } else {
-                        System.out.println(line);
-                    }
-                }*/
                 String line;
                 while((line = r.readLine()) != null){
                     System.out.println(line);
                 }
-                // ↓
             } catch(IOException e){
-                //失敗したときの処理がここ
                 e.printStackTrace();
             }
     }
